43957a369964a839b27f4da12831869ae291e8a2
Dante Pangan <dantepangan@gmail.com>
2016-01-14 14:31:36 +0800

    updates

--- application/modules/employee/views/cod/dashboard.php
+++ application/modules/employee/views/cod/dashboard.php
@@ -1,27 +1,47 @@
 <?php
-if (empty($cod_details)) {
-	echo "<center><h3>No Document Found.</h3><center>";	
-} else {
-?>
+// check if this shoud be accessible by current user
+$cod_access_setting = $this->setting->code_of_discipline_access_position_ids;
 
-	<div class="page-header clearfix">
-		<center><h2 style="color:#ffffff;border:1px solid;background:rgba(0, 0, 0, 0) linear-gradient(90deg, #0088CC 10%, #0B4B77 90%) repeat scroll 0 0">Code Of Discipline</small></h2></center>
-	</div>
+$position_id = explode("|", $cod_access_setting);
 
-	<div class="page-header">  
-	  <h2><?= $cod_details->cod_name; ?> <br/><small><?= $cod_details->cod_description; ?></small></h2>
-	</div>
+$is_allowed = 0;
 
-	<?php
-
-	echo "<iframe id='viewer' src='/assets/js/libs/ViewerJS/index.html#../../../media/cod/{$cod_details->asset_filename}' style='width:100%;height:800px;' allowfullscreen webkitallowfullscreen=''></iframe> <br/>";
-
-	echo "<br/>
-		  <div id='cod-comments'></div>
-		  <textarea placeholder='Put your comments here...' class='' style='width:67.5em;' id='new-cod-comment'></textarea>
-		  <button class='button-post btn btn-primary pull-right' style='margin-right:margin-bottom:10px;' data='{$cod_details->cod_id}' title='Post'>Post</button>	  
-		  ";
+foreach ($position_id as $pi) {
+	if ($pi == $this->employee->position_id) {
+		$is_allowed = 1;
+		break;
+	}
 }
+
+if (($is_allowed == 1) || ($cod_access_setting == "ALL")) {
+
+	if (empty($cod_details)) {
+		echo "<center><h3>No Document Found.</h3><center>";	
+	} else {
+	?>
+
+		<div class="page-header clearfix">
+			<center><h2 style="color:#ffffff;border:1px solid;background:rgba(0, 0, 0, 0) linear-gradient(90deg, #0088CC 10%, #0B4B77 90%) repeat scroll 0 0">Code Of Discipline</small></h2></center>
+		</div>
+
+		<div class="page-header">  
+		  <h2><?= $cod_details->cod_name; ?> <br/><small><?= $cod_details->cod_description; ?></small></h2>
+		</div>
+
+		<?php
+
+		echo "<iframe id='viewer' src='/assets/js/libs/ViewerJS/index.html#../../../media/cod/{$cod_details->asset_filename}' style='width:100%;height:800px;' allowfullscreen webkitallowfullscreen=''></iframe> <br/>";
+
+		echo "<br/>
+			  <div id='cod-comments'></div>
+			  <textarea placeholder='Put your comments here...' class='' style='width:67.5em;' id='new-cod-comment'></textarea>
+			  <button class='button-post btn btn-primary pull-right' style='margin-right:margin-bottom:10px;' data='{$cod_details->cod_id}' title='Post'>Post</button>	  
+			  ";
+	}
+} else {
+	echo "<center><h3><span style='color:red;'>Warning!</span> You are not allowed to directly access this page.<br/><small>Contact the IT Department if you need assistance. Thank you.</small></h3><center>";	
+}
+
 ?>
 
 <script type="text/javascript">
